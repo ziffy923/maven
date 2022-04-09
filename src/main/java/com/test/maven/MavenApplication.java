a60ee578f5af28c95b98e498eb1cb8e1f4e62446
ziffy923 <mahaprasad923@gmail.com>
2022-04-10 00:57:12 +0530

    First one

---
+++ src/main/java/com/test/maven/MavenApplication.java
@@ -0,0 +1,13 @@
+package com.test.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/maven/MavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
